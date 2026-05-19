94533317d58df785287baea4dd49e9756b571f1a
Ramyagulipalli3 <ramyagulipalli3@gmail.com>
2026-05-19 15:16:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function convertTemp(){
+
+    let temp = document.getElementById("temp").value;
+    let unit = document.getElementById("unit").value;
+    let result = document.getElementById("result");
+
+    if(temp === ""){
+        result.innerHTML = "Please enter temperature";
+        return;
+    }
+
+    if(unit === "celsius"){
+        let fahrenheit = (temp * 9/5) + 32;
+        result.innerHTML = temp + "°C = " + fahrenheit.toFixed(2) + "°F";
+    }
+    else{
+        let celsius = (temp - 32) * 5/9;
+        result.innerHTML = temp + "°F = " + celsius.toFixed(2) + "°C";
+    }
+}
